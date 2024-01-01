6be13fb720b994325c029d0d56c707791d6ec122
halitomer <khaledomrkhaled@gmail.com>
2024-01-01 21:55:33 +0300

    add back  and sign in button

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -24,93 +24,159 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: lightBlack,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+      ),
       backgroundColor: lightBlack,
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 200,
-          ),
-          Center(
-            child: Image.asset(
-              'image/assets/newl.png',
-              width: 130,
-              height: 130,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 150,
             ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          const Text(
-            'Create your account',
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const ContainerTextFiled(
-            child: TextField(
-              decoration: InputDecoration(
-                icon: Icon(
-                  Icons.mail,
-                  color: Colors.grey,
-                ),
-                hintText: 'Email',
-                border: InputBorder.none,
+            Center(
+              child: Image.asset(
+                'image/assets/newl.png',
+                width: 130,
+                height: 130,
               ),
             ),
-          ),
-          ContainerTextFiled(
-            child: TextField(
-              obscureText: _isObscure,
-              decoration: InputDecoration(
-                  icon: const Icon(
-                    Icons.lock,
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Create your account',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const ContainerTextFiled(
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.mail,
                     color: Colors.grey,
                   ),
+                  hintText: 'Email',
                   border: InputBorder.none,
-                  hintText: 'Password',
-                  suffixIcon: IconButton(
-                    padding: const EdgeInsetsDirectional.only(end: 1),
-                    icon: Icon(
-                      _isObscure ? Icons.visibility : Icons.visibility_off,
-                      color: _iconColor,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isObscure = !_isObscure;
-                        if (_iconColor == Colors.grey) {
-                          _iconColor = Colors.red;
-                        } else {
-                          _iconColor = Colors.grey;
-                        }
-                      });
-                    },
-                  )),
+                ),
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Checkbox(
-                  value: isChecked,
-                  checkColor: Colors.white,
-                  fillColor: MaterialStateProperty.all(Colors.red),
-                  onChanged: (value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  }),
-              const Text('remember me',
+            ContainerTextFiled(
+              child: TextField(
+                obscureText: _isObscure,
+                decoration: InputDecoration(
+                    icon: const Icon(
+                      Icons.lock,
+                      color: Colors.grey,
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'Password',
+                    suffixIcon: IconButton(
+                      padding: const EdgeInsetsDirectional.only(end: 1),
+                      icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off,
+                        color: _iconColor,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                          if (_iconColor == Colors.grey) {
+                            _iconColor = Colors.red;
+                          } else {
+                            _iconColor = Colors.grey;
+                          }
+                        });
+                      },
+                    )),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                    value: isChecked,
+                    checkColor: Colors.white,
+                    fillColor: MaterialStateProperty.all(Colors.red),
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    }),
+                const Text('remember me',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  'Sign Up',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'already have an account?',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
+                    fontSize: 15,
                     fontWeight: FontWeight.bold,
-                  )),
-            ],
-          ),
-        ],
+                  ),
+                ),
+                TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Sign in ',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
